9ffbda7891955ac6826bb1c0f272b9135b57c4b8
shubhankar_93 <official.shubhankar@gmail.com>
2025-03-07 00:02:23 +0530

    would be reverted

--- SeleniumJava/src/test/java/Test/ExampleRstore.java
+++ SeleniumJava/src/test/java/Test/ExampleRstore.java
@@ -25,6 +25,6 @@ public class ExampleRstore {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-		
+		//trying soft commit now
 	}
 }
